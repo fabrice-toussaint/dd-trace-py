8fb69005ab2d6822027736e28db81e47eecb10f5
Emmett Butler <emmett.butler321@gmail.com>
2026-06-18 11:08:38 -0700

    lint

--- ddtrace/appsec/_contrib/openai/handlers.py
+++ ddtrace/appsec/_contrib/openai/handlers.py
@@ -2,8 +2,8 @@ from typing import Any
 
 from ddtrace.appsec._asm_request_context import call_waf_callback
 from ddtrace.appsec._asm_request_context import in_asm_context
-from ddtrace.internal.utils import core
 from ddtrace.internal.logger import get_logger
+from ddtrace.internal.utils import core
 
 
 logger = get_logger(__name__)
